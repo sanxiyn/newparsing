65ae41d31755721d4193f1320cc675e95ee1f46e
Seo Sanghyeon <sanxiyn@gmail.com>
2011-01-02 12:47:30 +0900

    Classes

---
+++ newparsing.py
@@ -0,0 +1,27 @@
+class Parser(object):
+
+    pass
+
+class Empty(Parser):
+
+    pass
+
+class Null(Parser):
+
+    pass
+
+class Token(Parser):
+
+    pass
+
+class And(Parser):
+
+    pass
+
+class Or(Parser):
+
+    pass
+
+class Action(Parser):
+
+    pass
---
+++ test_newparsing.py
@@ -0,0 +1,10 @@
+from newparsing import *
+
+def test_class():
+    Parser
+    Empty
+    Null
+    Token
+    And
+    Or
+    Action
